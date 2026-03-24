8e30e0927e82fc0a243370cd3e4efad3a1877a6f
Arthur-PRZ <arthur.perez66000@gmail.com>
2026-03-24 13:29:15 +0100

    handling errors

--- srcs/requirements/mariadb/init-mariadb.sh
+++ srcs/requirements/mariadb/init-mariadb.sh
@@ -10,15 +10,16 @@ mysqld --user=mysql &
 sleep 5
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-#mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
+mysql -e "FLUSH PRIVILEGES;"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 sleep 2
 
-mkdir -p /run/mysqld
-chown mysql:mysql /run/mysqld
+mkdir -p /var/run/mysqld
+chown mysql:mysql /var/run/mysqld
 
 exec mysqld --user=mysql
