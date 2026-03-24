ec3f14dfaac612b92b766d28a8e7bbe26d6a2e80
Arthur-PRZ <arthur.perez66000@gmail.com>
2026-03-24 12:28:23 +0100

    handling errors

--- srcs/requirements/mariadb/init-mariadb.sh
+++ srcs/requirements/mariadb/init-mariadb.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #service mysql start;
 
-mkdir -p /run/mysqld
-chown mysql:mysql /run/mysqld
+mkdir -p /var/run/mysqld
+chown mysql:mysql /var/run/mysqld
 
 mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-mysql --user=mysql &
+mysqld --user=mysql &
 sleep 5
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+#mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
