bb80c05942762bbaae7cbb585ad2b7f4fed7e288
Arthur-PRZ <arthur.perez66000@gmail.com>
2026-03-24 11:47:02 +0100

    Docker compose done, handling errors

--- srcs/requirements/mariadb/init-mariadb.sh
+++ srcs/requirements/mariadb/init-mariadb.sh
@@ -9,11 +9,11 @@ mysql_install_db --user=mysql --datadir=/var/lib/mysql
 mysql --user=mysql &
 sleep 5
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`{SQL_DATABASE}\`;"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@<'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGIES;"
+mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 sleep 2
